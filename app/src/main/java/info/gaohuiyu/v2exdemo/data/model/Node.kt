51aedb004a31fc96ed59c202c9ad116f0d4f3da8
Huiyu Gao <beatbox_gao@hotmail.com>
2018-11-30 02:06:11 +0800

    jsoup demo

--- app/src/main/java/info/gaohuiyu/v2exdemo/data/model/Node.kt
+++ app/src/main/java/info/gaohuiyu/v2exdemo/data/model/Node.kt
@@ -1,19 +1,70 @@
 package info.gaohuiyu.v2exdemo.data.model
-import com.google.gson.annotations.SerializedName
 
-data class Node(
-    @SerializedName("avatar_large") val avatarLarge: String?, // //cdn.v2ex.com/navatar/c4ca/4238/1_large.png?m=1494924246
-    @SerializedName("name") val name: String?, // babel
-    @SerializedName("avatar_normal") val avatarNormal: String?, // //cdn.v2ex.com/navatar/c4ca/4238/1_normal.png?m=1494924246
-    @SerializedName("title") val title: String?, // Project Babel
-    @SerializedName("url") val url: String?, // https://www.v2ex.com/go/babel
-    @SerializedName("topics") val topics: Int?, // 1122
-    @SerializedName("footer") val footer: String?, // V2EX 基于 Project Babel 驱动。Project Babel 是用 Python 语言写成的，运行于 Google App Engine 云计算平台上的社区软件。Project Babel 当前开发分支 2.5。最新版本可以从 <a href="http://github.com/livid/v2ex" target="_blank">GitHub</a> 获取。
-    @SerializedName("header") val header: String?, // Project Babel - 帮助你在云平台上搭建自己的社区
-    @SerializedName("title_alternative") val titleAlternative: String?, // Project Babel
-    @SerializedName("avatar_mini") val avatarMini: String?, // //cdn.v2ex.com/navatar/c4ca/4238/1_mini.png?m=1494924246
-    @SerializedName("stars") val stars: Int?, // 378
-    @SerializedName("root") val root: Boolean?, // false
-    @SerializedName("id") val id: Int?, // 1
-    @SerializedName("parent_node_name") val parentNodeName: String? // v2ex
-)
+import org.jsoup.Jsoup
+
+data class Topic(val title: String?, val url: String?, val publisher: Member?,
+                 val node: Node?, val lastReplyTime: String?, val lastReplyMember: Member?,
+                 val replyCount: Int)
+data class Member(val avatar: String?, val name: String?)
+data class Node(val name: String?)
+
+fun main(args: Array<String>) {
+    val doc = Jsoup.connect("https://www.v2ex.com").get()
+    println("title: ${doc.title()}")
+//    val doc = Jsoup.parse(File("index.html"), "UTF-8", "https://www.v2ex.com/")
+//    println("title: ${doc.title()}")
+    val cellItems = doc.select("#Main > div.box > div.cell.item")
+
+    val topics = MutableList(cellItems.size) {
+        val tr = cellItems[it].select("tr")[0]
+        val memberA = tr.select("td:first-child > a")
+
+        val thirdTD = tr.select("td:nth-child(3)")
+
+        val titleA = thirdTD.select("span.item_title > a")
+        val topicInfo = thirdTD.select("span.topic_info")[0]
+
+        val lastReplyTime: String? = topicInfo.ownText().split("•").run {
+            if (this.size > 2) {
+                this[2]
+            } else {
+                null
+            }?.trim()
+        }
+
+        val lastReplyName: String? = topicInfo.run {
+            if (this.children().size > 3) {
+                child(3).select("strong > a").html()
+            } else {
+                null
+            }
+        }
+        val lastReplyMember = Member(null, lastReplyName)
+
+        val replyCount = tr.select("a.count_livid").text().run {
+            if (this.isNullOrEmpty()) {
+                0
+            } else {
+                this.toInt()
+            }
+        }
+
+        val title = titleA.html()
+        val topicUrl = titleA.attr("href")
+
+        val member: Member = memberA.let {
+            val memberName = it.attr("href")
+            val memberAvatar = it.select("img").attr("src")
+            Member(memberAvatar, memberName)
+        }
+
+        val nodeA = tr.select("a.node")
+        val node = Node(nodeA.html())
+
+        return@MutableList Topic(title, topicUrl, member, node, lastReplyTime, lastReplyMember, replyCount)
+    }
+
+    topics.forEach {
+        println(it)
+    }
+}
